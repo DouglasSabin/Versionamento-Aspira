027019604876534f53e7e0f3c5d6eb1260c6c4e8
Douglas Sabin Barbosa <dodo.sabin15@gmail.com>
2020-10-31 00:51:58 -0300

    alterado

--- exercicios1/3.js
+++ exercicios1/3.js
@@ -7,11 +7,7 @@ até 1.045,00 30%
 2.600,01 a 3.550,00 15%
 Acima de 3.550,00 10% */
 
-<<<<<<< HEAD
-//mais um comentario aleatorio
-=======
-//testando pela segunda vez
->>>>>>> bhanch-novo
+//TESTANdo pela segunda ou terceira vez
 
 var salario = 1100.50;
 
